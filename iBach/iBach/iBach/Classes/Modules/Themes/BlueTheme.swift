ad0b776779ef813b717d823622ea1750fd23d33b
neven travas <neven.travas@gmail.com>
2019-01-07 21:54:24 +0100

    modular themes updated

---
+++ iBach/iBach/iBach/Classes/Modules/Themes/BlueTheme.swift
@@ -0,0 +1,27 @@
+//
+//  BlueTheme.swift
+//  iBach
+//
+//  Created by Neven Travas on 12/12/2018.
+//  Copyright © 2018 Petar Jedek. All rights reserved.
+//
+
+import UIKit
+
+struct BlueTheme: Theme {
+    
+    
+    let tint: UIColor = .blue
+    
+    let backgroundColor: UIColor = UIColor(red: 135/255, green: 206/255, blue: 250/255, alpha: 1)
+    let separatorColor: UIColor = .darkGray
+    let selectionColor: UIColor = .lightText
+    let labelColor: UIColor = .black
+    let secondaryLabelColor: UIColor = .darkGray
+    var subtleLabelColor: UIColor = .lightGray
+    let textFieldColor: UIColor = .white
+    let buttonColor: UIColor = .blue
+    
+    
+    let barStyle: UIBarStyle = .default
+}
