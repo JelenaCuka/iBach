e80d18cd32c42a7ef880852b413964ec7806fb00
pjadek <38253652+pjadek@users.noreply.github.com>
2019-01-12 14:18:13 +0100

    fixes, playlists UI change, search view controller added to playlists

--- iBach/iBach/iBach/Classes/Modules/Playlists/PlaylistsTableViewController.swift
+++ iBach/iBach/iBach/Classes/Modules/Playlists/PlaylistsTableViewController.swift
@@ -8,19 +8,24 @@
 
 import UIKit
 import Unbox
-import CoreImage
 
-class PlaylistsTableViewController: UIViewController {
-
-    @IBOutlet var tableViewPlaylists: UITableView!
-    
+class PlaylistsTableViewController: UITableViewController {
     
     var playlistData: [Playlist] = []
     
+    let searchController = UISearchController(searchResultsController: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.tableFooterView = UIView()
         
         loadData()
+        
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
     }
     
     private func loadData() {
@@ -40,24 +45,21 @@ class PlaylistsTableViewController: UIViewController {
                         }
                     }
                 }
-                self.tableViewPlaylists.reloadData()
+                self.tableView.reloadData()
             })
         }
     }
     
-}
-
-extension PlaylistsTableViewController: UITableViewDelegate, UITableViewDataSource {
     
-    func numberOfSections(in tableView: UITableView) -> Int {
+    override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.playlistData.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cellIdentifier = "playlistCell"
         guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? PlaylistsTableViewCell else {
             fatalError("Error")
@@ -66,12 +68,18 @@ extension PlaylistsTableViewController: UITableViewDelegate, UITableViewDataSour
         if let imageURL = URL(string: self.playlistData[indexPath.row].coverArtUrl) {
             let color: UIColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.4)
             
-            cell.imageViewCoverArt.layer.cornerRadius = 25
+            cell.imageViewCoverArt.layer.cornerRadius = 10
             cell.imageViewCoverArt.clipsToBounds = true
             cell.imageViewCoverArt.layer.borderWidth = 0.5
             cell.imageViewCoverArt.layer.borderColor = color.cgColor
             
             cell.imageViewCoverArt.af_setImage(withURL: imageURL)
+            
+            let blur = UIBlurEffect(style: .light)
+            let blurView = UIVisualEffectView(effect: blur)
+            blurView.frame = cell.imageViewCoverArt.bounds
+            
+            cell.imageViewCoverArt.addSubview(blurView)
         }
         
         cell.labelName.text = self.playlistData[indexPath.row].name
@@ -79,7 +87,7 @@ extension PlaylistsTableViewController: UITableViewDelegate, UITableViewDataSour
         return cell
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         /*let playerItem = AVPlayerItem(url: URL(string: self.songData[indexPath.row].fileUrl)!)
          player = AVPlayer(playerItem: playerItem)
          
@@ -108,4 +116,11 @@ extension PlaylistsTableViewController: UITableViewDelegate, UITableViewDataSour
         //NotificationCenter.default.post(name: NSNotification.Name(rawValue: "sendSongList"), object: nil, userInfo: songsToPlay)
     }
     
+}
+
+extension PlaylistsTableViewController: UISearchResultsUpdating  {
+    // MARK: - UISearchResultsUpdating Delegate
+    func updateSearchResults(for searchController: UISearchController) {
+        //filterContentForSearchText(searchController.searchBar.text!)
+    }
 }
