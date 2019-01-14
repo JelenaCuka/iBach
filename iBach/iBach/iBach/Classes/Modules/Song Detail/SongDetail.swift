f4aab9924d33e47dc4accc8b5d63f363a583adfd
neven travas <neven.travas@gmail.com>
2019-01-14 20:59:50 +0100

    protocol added

---
+++ iBach/iBach/iBach/Classes/Modules/Song Detail/SongDetail.swift
@@ -0,0 +1,13 @@
+//
+//  SongDetail.swift
+//  iBach
+//
+//  Created by Neven Travaš on 14/01/2019.
+//  Copyright © 2019 Petar Jedek. All rights reserved.
+//
+
+import Foundation
+
+protocol SongDetail {
+    
+}
