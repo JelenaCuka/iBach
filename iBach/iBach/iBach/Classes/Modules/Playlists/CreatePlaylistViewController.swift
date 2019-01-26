2296ba08e534b15647b11035f9e8f83b8f78affd
Nikola Karan <nixa1995kt@gmail.com>
2019-01-26 22:38:15 +0100

    Dodan button Next koji je inicijalno onemogućen

--- iBach/iBach/iBach/Classes/Modules/Playlists/CreatePlaylistViewController.swift
+++ iBach/iBach/iBach/Classes/Modules/Playlists/CreatePlaylistViewController.swift
@@ -10,21 +10,16 @@ import UIKit
 
 class CreatePlaylistViewController: UIViewController {
 
+    
+    @IBOutlet weak var texfieldPlaylistName: UITextField!
+    
+    @IBOutlet weak var nextButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        nextButton.isEnabled = false
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
