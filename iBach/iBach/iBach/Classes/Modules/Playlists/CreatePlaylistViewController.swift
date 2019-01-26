ac13cfdd07bd4495d11e61cc408809dccda39258
Nikola Karan <nixa1995kt@gmail.com>
2019-01-26 22:31:59 +0100

    Kreiran novi view koji se otvara nakon klika na  plus u viewu dodan label i input field za unos imena nove playliste

---
+++ iBach/iBach/iBach/Classes/Modules/Playlists/CreatePlaylistViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreatePlaylistViewController.swift
+//  iBach
+//
+//  Created by Nikola on 26/01/2019.
+//  Copyright © 2019 Petar Jedek. All rights reserved.
+//
+
+import UIKit
+
+class CreatePlaylistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
